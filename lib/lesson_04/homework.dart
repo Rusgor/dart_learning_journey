f4a693ca80fbddc4149cecce4c0f3fabe2ec6302
Rusgor <it.rusgor@gmail.com>
2026-03-17 11:31:01 +0200

    DART-04: implement primitives()

---
+++ lib/lesson_04/homework.dart
@@ -0,0 +1,20 @@
+void main() {
+  primitives();
+  collections();
+  varFinalConst();
+  record();
+  nullable();
+}
+
+/// Завдання 1: Примітивні типи
+void primitives() {
+  int age = 25;
+  double height = 1.75;
+  String name = "Ruslan";
+  bool isStudent = true;
+
+  print("Age: $age");
+  print("Height: $height");
+  print("Name: $name");
+  print("Is student: $isStudent");
+}
