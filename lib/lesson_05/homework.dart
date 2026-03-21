c50d59e98aa32ecc48a4e9c031941bbc6f5b49d5
Rusgor <it.rusgor@gmail.com>
2026-03-21 13:19:36 +0200

    DART-05: create homework file and add initial setup

---
+++ lib/lesson_05/homework.dart
@@ -0,0 +1,14 @@
+/// Завдання 1: Математичні оператори
+void task1() {
+  final width = 18.4;
+  final height = 30.9;
+
+  final area = width * height;
+  final perimeter = 2 * (width + height);
+
+  print('Area: $area');
+  print('Perimeter: $perimeter');
+
+  final squares = area ~/ 1;
+  print('Full 1x1 squares: $squares');
+}
