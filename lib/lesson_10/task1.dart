65c7b2bea742668c60a21ba08c5cb9dff02221d0
Rusgor <it.rusgor@gmail.com>
2026-04-11 18:04:50 +0300

    feat: complete Task 1-2 (Future name + age)

--- lib/lesson_10/task1.dart
+++ lib/lesson_10/task1.dart
@@ -7,7 +7,30 @@ Future<String> fetchName() {
   );
 }
 
+Future<String> fetchAge() {
+  return Future.delayed(
+    const Duration(milliseconds: 1500),
+    () => '37',
+  );
+}
+
+String getYearWord(int age) {
+  if (age % 10 == 1 && age % 100 != 11) {
+    return 'рік';
+  } else if ([2, 3, 4].contains(age % 10) &&
+      ![12, 13, 14].contains(age % 100)) {
+    return 'роки';
+  } else {
+    return 'років';
+  }
+}
+
 void main() async {
   final name = await fetchName();
   print('Мене звати $name');
+
+  final ageString = await fetchAge();
+  final age = int.parse(ageString);
+
+  print('Мені $age ${getYearWord(age)}');
 }
